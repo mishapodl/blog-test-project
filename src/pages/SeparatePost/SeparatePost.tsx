789c948e563490ade5350b4c87c61611e8931ac0
Mikhail <mishapod@gmail.com>
2021-03-31 22:39:15 +0300

    separate post done

--- src/pages/SeparatePost/SeparatePost.tsx
+++ src/pages/SeparatePost/SeparatePost.tsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { FC, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useActions } from '../../hooks/useActions'
+import { usePost } from '../../hooks/usePost'
 import { useTypedSelector } from '../../hooks/useTypedSelector'
+import { IPost } from '../../types/posts'
+import { IComment } from './../../types/comments'
 
-export const SeparatePost = () => {
+export const SeparatePost: FC = () => {
   const { id }: any = useParams()
   const { fetchComments } = useActions()
+  const { specificPost } = usePost()
   const { comments } = useTypedSelector((state) => state.comments)
+  const post: IPost = specificPost(id)
 
   useEffect(() => {
     fetchComments(id)
@@ -14,8 +19,15 @@ export const SeparatePost = () => {
 
   return (
     <div>
+      {post && (
+        <div>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+          <span>{post.userId}</span>
+        </div>
+      )}
       <h1>Post ID: {id}</h1>
-      {comments.map(({ id, name, body }) => (
+      {comments.map(({ id, name, body }: IComment) => (
         <div key={id}>
           <h3>Name: {name}</h3>
           <p>{body}</p>
